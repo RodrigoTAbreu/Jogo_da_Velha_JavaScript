1f2c0df4ad864f03068d49f3c352f088275531ce
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2020-10-14 06:49:01 -0300

    Palco do Jogo

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var jogador = null;
+
+function escolherquadrado(id){ /* apontamos ID no parametro para que será capturado ao clicar -- para iosso usamos o ONCLICK(THIS.ID) no html*/
+    console.log(id) /*o ID em console.log faz com que ele mostre qual é o id do eelemento que foi clicado  */
+}
